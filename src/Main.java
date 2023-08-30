09f9bf5f8fe708f5e1008c43bc496e3e3e417ee5
IQA69 <charlesdward@mail.ru>
2023-08-30 22:32:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int prise = 13_676;
+
+        int bonus = prise / 20;
+        System.out.println("Бонус: " + bonus);
+    }
+}
